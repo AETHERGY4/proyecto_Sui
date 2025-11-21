b05288fdd190a961f55281033c0ebd360b55d385
AETHERGY4 <analiliaramosln123@gmail.com>
2025-11-21 06:31:09 +0000

    proc3

--- Proyect1/src/networkConfig.js
+++ Proyect1/src/networkConfig.js
@@ -2,14 +2,14 @@ import { getFullnodeUrl } from "@mysten/sui/client";
 import { PACKAGE_ID } from "./constants.js";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
-const { networkConfig, useNetworkVariable, useNetworkVariables } = 
-    createNetworkConfig({
-        testnet: {
-            url: getFullnodeUrl("testnet"),
-            variables: {
-                packageId: PACKAGE_ID,
-            },
-        },
-    });
+const { networkConfig, useNetworkVariable, useNetworkVariables } =
+  createNetworkConfig({
+    testnet: {
+      url: getFullnodeUrl("testnet"),
+      variables: {
+        PackageId: PACKAGE_ID,
+      },
+    },
+  });
 
 export { useNetworkVariable, useNetworkVariables, networkConfig };
